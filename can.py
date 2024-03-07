d1705672f6d9c4095944bfaf84844c80765feab5
Ben Kollar <benjkollar03@gmail.com>
2024-03-07 18:36:39 +0000

    commit changes

--- can.py
+++ can.py
@@ -10,8 +10,10 @@ def handler_stop_signals(signum, frame):
 
 signal.signal(signal.SIGINT, handler_stop_signals)
 signal.signal(signal.SIGTERM, handler_stop_signals)
+
 os.system('sudo ip link set can0 type can bitrate 100000')
 os.system('sudo ifconfig can0 up')
+
 can0 = can.interface.Bus(channel = 'can0', bustype = 'socketcan')# socketcan_native
 while run:
     msg = can0.recv(5.0)
